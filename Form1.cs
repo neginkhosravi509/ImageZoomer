37ddd338e475636ca88b28697126974f8dfc4e78
negin <neginkhosravi509@gmail.com>
2025-11-20 23:45:17 +0330

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Width += 4;
+            pictureBox1.Height += 4;
+            pictureBox1.Left = (this.Width - pictureBox1.Width) / 2;
+            pictureBox1.Top = (this.Height - pictureBox1.Height) / 2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Width -= 4;
+            pictureBox1.Height -= 4;
+            pictureBox1.Left = (this.Width - pictureBox1.Width) / 2;
+            pictureBox1.Top = (this.Height - pictureBox1.Height) / 2;
+        }
+    }
+}
